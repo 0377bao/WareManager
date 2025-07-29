5c009592d4fc2203148a79e099ffce0bdc3b5253
manhhieu120499 <vomanhhieu62@gmail.com>
2025-07-29 15:59:40 +0700

    Component: custom Wrapper Tippy

---
+++ frontend/src/components/Menu/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import WrapperTippy from '../WrapperTippy';
+import Popper from '../Popper';
+import classNames from 'classnames/bind';
+import styles from './Menu.module.scss';
+import MenuItem from './MenuItem';
+
+
+const cx = classNames.bind(styles);
+
+const Menu = ({ children, menuItems = [] }) => {
+    const renderMenuItem = (attrs) => {
+        return (
+            <div ref={attrs.ref} className={cx('menu-list')} tabIndex="-1" {...attrs}>
+                <Popper classname={cx('menu-list')}>
+                    {menuItems.length > 0 &&
+                        menuItems.map((item, index) => (
+                            <MenuItem key={index} title={item.title} Icon={item.Icon} path={item.path} />
+                        ))}
+                </Popper>
+            </div>
+        );
+    };
+
+    return <WrapperTippy renderTooltip={renderMenuItem}>{children}</WrapperTippy>;
+};
+
+export default Menu;
