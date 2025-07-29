5c171fdc8ed5baa5ee7e633ab87c2c645eaa795f
manhhieu120499 <vomanhhieu62@gmail.com>
2025-07-29 15:56:43 +0700

    Modify: edit fontSize sidebar-item and change icon

--- frontend/src/layouts/components/Sidebar/index.jsx
+++ frontend/src/layouts/components/Sidebar/index.jsx
@@ -1,82 +1,94 @@
-import React from "react";
-import classNames from "classnames/bind";
-import styles from "./Sidebar.module.scss";
-import SidebarItem from "./SidebarItem";
-import { faSquareCheck, faChartBar, faEnvelope, faUser, faFileExcel, faArrowAltCircleLeft, faArrowAltCircleRight, faLemon, faFile } from "@fortawesome/free-regular-svg-icons";
-import logo from "../../../assets/logo.png"
-import { useLocation } from "react-router-dom";
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+import SidebarItem from './SidebarItem';
+import {
+    Settings,
+    User,
+    LayoutDashboard,
+    ShoppingBasket,
+    CircleArrowLeft,
+    CircleArrowRight,
+    ClipboardMinus,
+    Undo2,
+    CalendarCheck,
+} from 'lucide-react';
+import logo from '../../../assets/logo.png';
+import { useLocation } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
 const Sidebar = () => {
-  const sidebarMenu = [
-    {
-      title: "Dashboard",
-      iconName: faChartBar,
-      path: "/",
-    },
-    {
-      title: "Sản phẩm",
-      iconName: faLemon,
-      path: "/products",
-    },
-    {
-      title: "Nhập kho",
-      iconName: faArrowAltCircleRight,
-      path: "/ware-receive",
-    },
-    {
-      title: "Xuất kho",
-      iconName: faArrowAltCircleLeft,
-      path: "/ware-release",
-    },
-    {
-      title: "Kiểm kê kho",
-      iconName: faSquareCheck,
-      path: "/check-inventory",
-    },
-    {
-      title: "Báo cáo",
-      iconName: faFileExcel,
-      path: "/report",
-    },
-    {
-      title: "Đổi trả",
-      iconName: faFile,
-      path: "/return-order",
-    },
-    {
-      title: "Phân quyền",
-      iconName: faUser,
-      path: "/auth",
-    },
-    {
-      title: "Cài đặt",
-      iconName: faEnvelope,
-      path: "/settings",
-    },
-  ];
+    const sidebarMenu = [
+        {
+            title: 'Dashboard',
+            iconName: LayoutDashboard,
+            path: '/',
+        },
+        {
+            title: 'Sản phẩm',
+            iconName: ShoppingBasket,
+            path: '/products',
+        },
+        {
+            title: 'Nhập kho',
+            iconName: CircleArrowRight,
+            path: '/ware-receive',
+        },
+        {
+            title: 'Xuất kho',
+            iconName: CircleArrowLeft,
+            path: '/ware-release',
+        },
+        {
+            title: 'Kiểm kê kho',
+            iconName: CalendarCheck,
+            path: '/check-inventory',
+        },
+        {
+            title: 'Báo cáo',
+            iconName: ClipboardMinus,
+            path: '/report',
+        },
+        {
+            title: 'Đổi trả',
+            iconName: Undo2,
+            path: '/return-order',
+        },
+        {
+            title: 'Phân quyền',
+            iconName: User,
+            path: '/auth',
+        },
+        {
+            title: 'Cài đặt',
+            iconName: Settings,
+            path: '/settings',
+        },
+    ];
 
-  let location = useLocation();
-  return (
-    <div className={cx("wrapper-sidebar")}>
-      <div className={cx("sidebar-content")}>
-        <div className={cx("info-user")}>
-          <img src={logo} className={cx('logo-sidebar')} loading="lazy"/>
-          <h1 className={cx("username")}>Nguyen Van Minh</h1>
+    let location = useLocation();
+    return (
+        <div className={cx('wrapper-sidebar')}>
+            <div className={cx('sidebar-content')}>
+                <div className={cx('info-user')}>
+                    <img src={logo} className={cx('logo-sidebar')} loading="lazy" />
+                    <h1 className={cx('username')}>Nguyen Van Minh</h1>
+                </div>
+                <div className={cx('sidebar-list')}>
+                    {sidebarMenu.map((item, index) => (
+                        <SidebarItem
+                            key={index}
+                            title={item.title}
+                            iconName={item.iconName}
+                            path={item.path}
+                            location={location.pathname}
+                        />
+                    ))}
+                </div>
+            </div>
         </div>
-        <div className={cx("sidebar-list")}>
-            {sidebarMenu.map((item, index) => <SidebarItem
-            key={index}
-            title={item.title}
-            iconName={item.iconName}
-            path={item.path}
-            location={location.pathname}
-          />)}
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Sidebar;
