4b486e2788b08889cb6399b7579413d1040d5f70
manhhieu120499 <vomanhhieu62@gmail.com>
2025-07-31 11:02:57 +0700

    Modify: edit animation chatbox

--- frontend/src/components/ChatBox/index.jsx
+++ frontend/src/components/ChatBox/index.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './ChatBox.module.scss';
+import { X as XClosed } from 'lucide-react';
+
 
 const cx = classNames.bind(styles);
 
-const ChatBox = ({ classnames, isOpen }) => {
+const ChatBox = ({ classnames, isOpen, closed }) => {
     return (
         <div
             className={cx('chat-box', {
                 [classnames]: isOpen,
             })}
         >
-            <h1 className={cx('header-chat-box')}>Trợ lý thông minh</h1>
+            <div className={cx('header-chat-box')}>
+                <h1 className={cx('header-title')}>Trợ lý thông minh</h1>
+                <button className={cx('btn-close-chat')} onClick={closed}>
+                    <XClosed size={20}/>
+                </button>
+            </div> 
             <div className={cx('content-chat-box')}>Nội dung</div>
             {/*input search chat box*/}
         </div>
