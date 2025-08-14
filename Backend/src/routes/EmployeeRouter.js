05e6c6811a36c8b2481cd4644ba36592d3e8d91f
manhhieu120499 <vomanhhieu62@gmail.com>
2025-08-14 21:58:31 +0700

    push api employee

--- Backend/src/routes/EmployeeRouter.js
+++ Backend/src/routes/EmployeeRouter.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const { authUser } = require('../middleware/AuthMiddleware');
+const validate = require('../validates/validate');
+const EmployeeController = require('../controllers/EmployeeController');
+const { checkEmail } = require('../validates/employee.validation');
 const router = express.Router();
-router.get("/", (req, res) => {
-    return res.send("EmployeeRouter Router is working!");
+router.get('/', (req, res) => {
+    return res.send('EmployeeRouter Router is working!');
 });
 
+router.post('/employee-detail', authUser, checkEmail, validate, EmployeeController.getEmployee);
 module.exports = router;
---
+++ Backend/src/services/EmployeeService.js
@@ -0,0 +1,41 @@
+const db = require('../../models');
+const dotenv = require('dotenv');
+const Employee = db.Employee;
+
+dotenv.config();
+
+const HTTP_OK = process.env.HTTP_OK;
+const HTTP_NOT_FOUND = process.env.HTTP_NOT_FOUND;
+const HTTP_INTERNAL_SERVER_ERROR = process.env.HTTP_INTERNAL_SERVER_ERROR;
+
+class EmployeeService {
+    findEmployee(employeeID) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const employee = await Employee.findOne({ employeeID });
+                if (employee)
+                    resolve({
+                        status: 'OK',
+                        statusHttp: HTTP_OK,
+                        message: 'Lấy nhân viên thành công',
+                        employee,
+                    });
+                else {
+                    reject({
+                        status: 'OK',
+                        statusHttp: HTTP_NOT_FOUND,
+                        message: 'Nhân viên không tồn tại',
+                    });
+                }
+            } catch (err) {
+                reject({
+                    status: 'ERR',
+                    statusHttp: HTTP_INTERNAL_SERVER_ERROR,
+                    message: err,
+                });
+            }
+        });
+    }
+}
+
+module.exports = new EmployeeService();
