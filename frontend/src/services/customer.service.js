92a5ca53d8c1eb3d1dfabf04b1768c16b8cbe4f5
Hiếu <vomanhhieu62@gmail.com>
2025-11-22 18:12:51 +0700

    update service

--- frontend/src/services/customer.service.js
+++ frontend/src/services/customer.service.js
@@ -1,10 +1,65 @@
-import request from "../utils/httpRequest";
+import { styleMessage } from '../constants';
+import request from '../utils/httpRequest';
+import toast from 'react-hot-toast';
+import parseToken from '../utils/parseToken';
+
 export const searchCustomer = async (customerID) => {
-    try{
+    try {
         const res = await request.get(`/api/customer/find/${customerID}`);
-        return res?.data?.data || null
-    }catch(error){
-        console.error("Error searching customer:", error);
-        throw new Error(error.response.data)
+        return res?.data?.data || null;
+    } catch (error) {
+        console.error('Error searching customer:', error);
+        throw new Error(error.response.data);
     }
-}
+};
+
+export const getAllCustomer = async () => {
+    try {
+        const res = await request.get('/api/customer/list');
+        return res?.data?.data || [];
+    } catch (err) {
+        console.log(err);
+        toast.error(
+            Array.isArray(err.response.data.message) ? err.response.data.message[0] : err.response.data.message,
+            styleMessage,
+        );
+        return err;
+    }
+};
+
+export const filterCustomer = async (optionFilter = {}) => {
+    try {
+        const res = await request.post('/api/customer/filter', {
+            ...optionFilter,
+        });
+        return res?.data?.data || [];
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+};
+
+export const fetchListHistoryOrderCustomer = async (customerID, page = 1) => {
+    try {
+        const token = parseToken('tokenUser');
+        const warehouse = parseToken('warehouse');
+        const res = await request.post(
+            `/api/customer/history-customer`,
+            {
+                customerID: customerID,
+                page,
+            },
+            {
+                headers: {
+                    token: `Beare ${token.accessToken}`,
+                    employeeid: token.employeeID,
+                    warehouseid: warehouse.warehouseID,
+                },
+            },
+        );
+        return res.data || null;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+};
